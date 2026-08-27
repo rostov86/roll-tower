b9ecfd370b52cc1417a293d6d3f48d5efb5a5bd1
rostov86 <rostov86@users.noreply.github.com>
2026-08-27 10:08:53 +0000

    Deploy GitHub Pages

---
+++ sw.js
@@ -0,0 +1,43 @@
+/* Offline cache for Башня роллов */
+const CACHE = 'roll-tower-v1'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll([
+        './',
+        './index.html',
+        './manifest.webmanifest',
+        './icons/icon-192.png',
+        './icons/icon-512.png',
+      ]).catch(() => undefined),
+    ),
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))),
+    ),
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request
+  if (req.method !== 'GET') return
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const network = fetch(req)
+        .then((res) => {
+          const copy = res.clone()
+          caches.open(CACHE).then((cache) => cache.put(req, copy)).catch(() => undefined)
+          return res
+        })
+        .catch(() => cached)
+      return cached || network
+    }),
+  )
+})
